d7edb6832d47ba11e2fb9214e1a7459a685b13da
Cpietro87 <cristianpietro2019@gmail.com>
2023-08-24 20:34:51 -0300

    instancia de un estado

--- src/compontes/Repaso/IntanciaStado.tsx
+++ src/compontes/Repaso/IntanciaStado.tsx
@@ -8,9 +8,9 @@ export default function Contador() {
       <h1>{number}</h1>
       <button
         onClick={() => {
-          setNumber((n) => n + 1);
-          setNumber((n) => n + 1);
-          setNumber((n) => n + 1);
+          setNumber(number + 1);
+          setNumber(number + 1);
+          setNumber(number + 1);
         }}
       >
         +3
